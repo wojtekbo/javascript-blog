dc59ad3336e4f943f05568b12c9be8a35337024e
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2021-12-14 22:52:50 +0100

    Add class 'active' to the clicked link in titleClickHandler

--- js/script.js
+++ js/script.js
@@ -6,21 +6,24 @@
 const titleClickHandler = function (event) {
    console.log("Link was clicked!");
    console.log(event);
+   const clickedElement = this;
+   console.log("clickedElement:", clickedElement);
+   console.log("clickedElement (with plus): " + clickedElement);
 
-   /* remove class 'active' from all article links  */
+   /* [DONE] remove class 'active' from all article links  */
    const activeLinks = document.querySelectorAll(".titles a.active");
 
    for (let activeLink of activeLinks) {
       activeLink.classList.remove("active");
    }
-   /* add class 'active' to the clicked link */
+   /* [IN PROGRESS] add class 'active' to the clicked link */
+   clickedElement.classList.add("active");
+   /* [DONE] remove class 'active' from all articles */
    const activeArticles = document.querySelectorAll(".post.active");
 
    for (let activeArticle of activeArticles) {
       activeArticle.classList.remove("active");
    }
-   /* remove class 'active' from all articles */
-
    /* get 'href' attribute from the clicked link */
 
    /* find the correct article using the selector (value of 'href' attribute) */
